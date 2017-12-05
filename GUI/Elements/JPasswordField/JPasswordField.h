76974b0e5d4f32a257c507e3f97b86f22fdad003
OSmile <32322253+OSmile@users.noreply.github.com>
2017-12-06 02:18:33 +0300

    Password with comments

--- GUI/Elements/JPasswordField/JPasswordField.h
+++ GUI/Elements/JPasswordField/JPasswordField.h
@@ -1,33 +1,34 @@
-#pragma once
+﻿#pragma once
 #include "../../GUI/GUIBox.h"
 
 class PasswordField : public GUIBox
 {
 private:
 	friend class GUILayer;
-	std::string text;
-	std::string textIn;
-	Text txt;         
-	std::string textButt; 
-	Text txtButt;         
-	RectangleShape field;
-	RectangleShape fieldButt;
-	std::vector<char> vect;
-	std::string correct;
-	bool buttPass = false;
-	bool fieldPass = false;
+	std::string text; ///< Ввод строки в поле пароля
+	std::string textIn; ///< Исходная строка, которая задается при вызове
+	Text txt;  ///< Вывод строки поля пароля          
+	std::string textButt; ///< Ввод строки в поле кнопки
+	Text txtButt;  ///< Вывод строки поля пароля        
+	RectangleShape field; ///< Область для поля пароля
+	RectangleShape fieldButt; ///< Область для поля кнопки
+	std::vector<char> vect; ///< Вектор для набранных символов
+	std::string correct; ///< Правильный пароль, задается при вызове
+	bool buttPass = false; ///< Проверка на то, нажата ли ЛКМ в поле кнопки
+	bool fieldPass = false; ///< Проверка на то, нажата ли ЛКМ в поле пароля
 
 protected:
+	/// Конструктор
 	PasswordField(RenderWindow* renderWindow_, float x, float y, float width, float height, std::string text, TextStyle *tstyle, GUIStyle *gstyle, std::string correct);
-	bool inField;
-	bool inFieldButt;
+	bool inField; ///< Координаты мыши в поле пароля, если true 
+	bool inFieldButt; ///< Координаты мыши в поле кнопки, если true
 
 public:
-	virtual void Draw() override;
-	virtual void Recalc() override;
-	Text Display();
-	Text DisplayButt();
-	void handleEvent(const sf::Event& event) override;
-	bool Inside(Vector2i); 
-	bool InsideButt(Vector2i);
+	virtual void Draw() override; ///< Переопределение функции Draw()
+	virtual void Recalc() override; ///< Переопредение функции Recalc()
+	Text Display();  ///< Вывод текста в поле пароля
+	Text DisplayButt(); ///< Вывод текста в поле кнопки
+	void handleEvent(const sf::Event& event) override; ///< Обработчик событий. Нажатие и печать текста.
+	bool Inside(Vector2i); ///< Проверка вхождения мыши в поле пароля
+	bool InsideButt(Vector2i); ///< Проверка вхождения мыши в поле кнопки
 };
